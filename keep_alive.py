6d0d56384d8d811b686baa9cc88417ecea34d9cf
elio27 <elio27online@gmail.com>
2021-02-03 11:49:42 +0000

    Minor improvements

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask("")
 
 @app.route('/')
 def home():
-  return "Hello, I'm alive !"
+  return "A man who kills at the front is a war hero, but if he kills out of passion, is he a criminal?"
 
 def run():
   app.run(host='0.0.0.0', port=8080)
