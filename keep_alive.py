9584c8ad7abd7bfdbbc90fce63b9921ffc9c7141
elio27 <elio27online@gmail.com>
2021-02-04 16:18:24 +0000

    Don't even now what changed this time

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask("")
 
 @app.route('/')
 def home():
-  return "A man who kills at the front is a war hero, but if he kills out of passion, is he a criminal?"
+  return '<head><title>RoutineBot</title><style>body{background:black;color:white}</style></head><body><p>A man who kills at the front is a war hero, but if he kills out of passion, is he a criminal?</p></body>'
 
 def run():
   app.run(host='0.0.0.0', port=8080)
