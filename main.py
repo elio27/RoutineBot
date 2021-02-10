42337aacb68ae9ec9bf4e9b764692406cdaacf36
elio27 <elio27online@gmail.com>
2021-02-10 16:59:02 +0000

    Added aiohttp and therefore decreased the response time !

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re, os, requests, json
 from keep_alive import keep_alive
 import difflib
 import html
+import aiohttp
 from PIL import Image
 from io import BytesIO
 
@@ -73,9 +74,10 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
 	channel = client.get_channel(503976650996842507)
-	await channel.send(
-	    f"Welcome <@{member.id}> and thank you for joining us! We are now {member.guild.member_count} in this server !"
-	)
+	if channel.guild == member.guild:
+	  await channel.send(
+	      f"Welcome <@{member.id}> and thank you for joining us! We are now {member.guild.member_count} in this server !"
+	  )
 
 
 @client.event
@@ -100,202 +102,205 @@ async def on_raw_reaction_add(payload):
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    if message.content and message.content.endswith(")"):
-      await message.add_reaction("🗑")
-    else:
-      if len(message.embeds) > 0  and not message.content == "https://bit.ly/rh-bot":
+  async with aiohttp.ClientSession() as session:
+    if message.author == client.user:
+      if message.content and message.content.endswith(")"):
         await message.add_reaction("🗑")
-  if message.content == "!rh help":
-    embed = discord.Embed(
-    colour = discord.Colour(0x299d68),
-    description="Hello and welcome to the RoutineBot help page !")
-    embed.set_footer(
-      text=f"{footer(message)}",
-      icon_url=
-      "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
-    embed.add_field(name="**Main Commands :**",value= "**!rh shortcut {RH ID / Name}**\n*Returns the author, amount of downloads and heart of a shortcut, the last version, the release date and a direct link to download it*\n\n**!rh search {shortcut's name}**\n*Returns a list of 5 results and their id for a or several keywords*\n\n**!rh user {username}**\n*Return the number of shortcuts, downloads and hearts of a shortcut creator.*\n\n")
-
-    embed.add_field(
-        name="\n\n**Other commands :**",
-        value=
-        "\n\n**!rh invite**\n*Returns the [link](https://bit.ly/rh-bot) to invite me on your server !*\n\n**!rh help**\n*Returns the help page you are reading*\n\n\n"
-    )
-    embed.add_field(name="**Credits :**", value="Here is a little message to thank those who contributed to RoutineBot, one way or another, by reporting bugs or suggested improvements.\nI am talking about <@360097325730889730>, <@575317218955493376>, <@618082876730376202>, <@714777087801819237>, <@642358695652753418> and <@293502958950154240>.")
-    embed.add_field(name="Other links:", value="[Github repo](https://github.com/elio27/RoutineBot)")
-    await message.channel.send(embed=embed)
-
-  elif message.content.startswith("!rh invite"):
-    await message.channel.send("https://bit.ly/rh-bot\n")
-
-  elif message.content.startswith("!rh search"):
-    name = message.content.replace("!rh search ", "")
-    loading = await message.channel.send("Routinebot is loading your request")
-    async with message.channel.typing():
-      mess = search(name, mode=1)
-      await loading.delete()
-      embed = discord.Embed(colour=discord.Colour(0x299d68))
+      else:
+        if len(message.embeds) > 0  and not message.content == "https://bit.ly/rh-bot":
+          await message.add_reaction("🗑")
+    if message.content == "!rh help":
+      embed = discord.Embed(
+      colour = discord.Colour(0x299d68),
+      description="Hello and welcome to the RoutineBot help page !")
       embed.set_footer(
-            text=f"{footer(message)}",
-            icon_url=
-            "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
-      if not f"RoutineBot cannot find any result for {name}" in mess:
-        embed.add_field(name=f"Results for {name} :",value=mess)
-      else:
-        embed.add_field(name="<:error:805061336185831434> Error <:error:805061336185831434>",value=mess)
-      
-    await message.channel.send(embed=embed)
- 
+        text=f"{footer(message)}",
+        icon_url=
+        "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
+      embed.add_field(name="**Main Commands :**",value= "**!rh shortcut {RH ID / Name}**\n*Returns the author, amount of downloads and heart of a shortcut, the last version, the release date and a direct link to download it*\n\n**!rh search {shortcut's name}**\n*Returns a list of 5 results and their id for a or several keywords*\n\n**!rh user {username}**\n*Return the number of shortcuts, downloads and hearts of a shortcut creator.*\n\n")
 
-  elif message.content.startswith("!rh shortcut"):
-    rhid = message.content.replace("!rh shortcut ", "")
-    loading = await message.channel.send("Routinebot is loading your request")
-    async with message.channel.typing():
-      try:
-        int(rhid)
-      except ValueError:
-        rhid = search(rhid)
-      if rhid and is_valid(rhid):
-        response = requests.get(f"https://routinehub.co/shortcut/{rhid}")
-        ex = "(?<=<p>Downloads: ).*(?=</p>)"
-        downloads = re.findall(ex, response.text)[0]
-        name = html.unescape(get_name(rhid))
-        ex = '(?<=<span class="heart-count">).*(?=</span>)'
-        hearts = re.findall(ex, response.text)[0]
+      embed.add_field(
+          name="\n\n**Other commands :**",
+          value=
+          "\n\n**!rh invite**\n*Returns the [link](https://bit.ly/rh-bot) to invite me on your server !*\n\n**!rh help**\n*Returns the help page you are reading*\n\n\n"
+      )
+      embed.add_field(name="**Credits :**", value="Here is a little message to thank those who contributed to RoutineBot, one way or another, by reporting bugs or suggested improvements.\nI am talking about <@360097325730889730>, <@575317218955493376>, <@618082876730376202>, <@714777087801819237>, <@642358695652753418> and <@293502958950154240>.")
+      embed.add_field(name="Other links:", value="[Github repo](https://github.com/elio27/RoutineBot)")
+      await message.channel.send(embed=embed)
 
-        ex = '(?<=<a href="\/user\/).*(?=">)'
-        author = re.findall(ex, response.text)[0]
+    elif message.content.startswith("!rh invite"):
+      await message.channel.send("https://bit.ly/rh-bot\n")
 
-        response = requests.get(
-            f"https://routinehub.co/api/v1/shortcuts/{rhid}/versions/latest"
-        )
-        json_data = json.loads(response.text)
-        last_version = json_data["Version"]
-        release_date = json_data["Release"]
-        url = json_data["URL"]
-        embed = discord.Embed(
-            colour=discord.Colour(0x299d68))
-        icon_url = url.replace("/shortcuts/", "/shortcuts/api/records/")
-
-        try:
-          icon_url = json.loads(requests.get(icon_url).text)["fields"]["icon"]["value"]["downloadURL"]
-        except:
-          icon_url = "https://media.discordapp.net/attachments/752322547877675058/805136237844234290/Sa6a0Ia.png"
-
-        riri = requests.get(icon_url)
-        i = Image.open(BytesIO(riri.content))
-        i.save("img.png", format=None)
-        file = discord.File("img.png", filename="img.png")
-        embed.set_thumbnail(url="attachment://img.png")
-
+    elif message.content.startswith("!rh search"):
+      name = message.content.replace("!rh search ", "")
+      loading = await message.channel.send("Routinebot is loading your request")
+      async with message.channel.typing():
+        mess = search(name, mode=1)
+        await loading.delete()
+        embed = discord.Embed(colour=discord.Colour(0x299d68))
         embed.set_footer(
-            text=f"{footer(message)}",
-            icon_url=
-            "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512"
-        )
+              text=f"{footer(message)}",
+              icon_url=
+              "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
+        if not f"RoutineBot cannot find any result for {name}" in mess:
+          embed.add_field(name=f"Results for {name} :",value=mess)
+        else:
+          embed.add_field(name="<:error:805061336185831434> Error <:error:805061336185831434>",value=mess)
+        
+      await message.channel.send(embed=embed)
+  
 
-        embed.add_field(
-            name=f"{name}",
-            value=
-            f"Author: [{author}](https://routinehub.co/user/{author})\nHearts: {hearts}\nDownloads: {downloads}\nLast version: {last_version}\nPublished on : {release_date}\n\n[Download {name}]({url})"
-        )
-        await loading.delete()
-        await message.channel.send(file=file, embed=embed)
-        os.remove("img.png")
+    elif message.content.startswith("!rh shortcut"):
+      rhid = message.content.replace("!rh shortcut ", "")
+      loading = await message.channel.send("Routinebot is loading your request")
+      async with message.channel.typing():
+        try:
+          int(rhid)
+        except ValueError:
+          rhid = search(rhid)
+        if rhid and is_valid(rhid):
 
-      else:
-        await loading.delete()
-        embed = discord.Embed(colour=discord.Colour(0x690a6))
+          async with session.get(f"https://routinehub.co/shortcut/{rhid}") as response:
+            ex = "(?<=<p>Downloads: ).*(?=</p>)"
+            downloads = re.findall(ex, await response.text())[0]
 
-        embed.set_footer(text=footer(message), icon_url="https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
+            ex = '(?<=<h3 class="title is-3">).*(?=</h3>)'
+            name = html.unescape(re.findall(ex, await response.text())[0])
 
-        embed.add_field(name="<:error:805061336185831434> Error <:error:805061336185831434>", value="This shortcut does not exist.\n\n")
+            ex = '(?<=<span class="heart-count">).*(?=</span>)'
+            hearts = re.findall(ex, await response.text())[0]
 
-        await message.channel.send(embed=embed)
+            ex = '(?<=<a href="\/user\/).*(?=">)'
+            author = re.findall(ex, await response.text())[0]
 
-  elif message.content.startswith("!rh user"):
-    user = message.content.replace("!rh user ", "")
-    if user:
-      user_url = f"https://routinehub.co/user/{user}"
-      response = requests.get(user_url)
-      if not "Error: Profile not found" in response.text:
+          async with session.get(f"https://routinehub.co/api/v1/shortcuts/{rhid}/versions/latest") as response:
+            json_data = json.loads(await response.text())
+            last_version = json_data["Version"]
+            release_date = json_data["Release"]
+            url = json_data["URL"]
+            embed = discord.Embed(
+                colour=discord.Colour(0x299d68))
+            icon_url = url.replace("/shortcuts/", "/shortcuts/api/records/")
 
-        loading = await message.channel.send("Routinebot is loading your request")
+            try:
+              icon_url = json.loads(requests.get(icon_url).text)["fields"]["icon"]["value"]["downloadURL"]
+            except:
+              icon_url = "https://media.discordapp.net/attachments/752322547877675058/805136237844234290/Sa6a0Ia.png"
 
-        async with message.channel.typing():
+          riri = requests.get(icon_url)
+          i = Image.open(BytesIO(riri.content))
+          i.save("img.png", format=None)
+          file = discord.File("img.png", filename="img.png")
+          embed.set_thumbnail(url="attachment://img.png")
 
-          ex = "(?<=<p>Shortcuts: ).*(?=</p>)"
-          sc = re.findall(ex, response.text)[0]
+          embed.set_footer(
+              text=f"{footer(message)}",
+              icon_url=
+              "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512"
+          )
 
-          ex = "(?<=<strong>).*(?=</strong>)"
-          username = re.findall(ex, response.text)[0]
+          embed.add_field(
+              name=f"{name}",
+              value=
+              f"Author: [{author}](https://routinehub.co/user/{author})\nHearts: {hearts}\nDownloads: {downloads}\nLast version: {last_version}\nPublished on : {release_date}\n[View full page on routinehub](https://routinehub.co/shortcut/{rhid})\n\n[Download {name}]({url})"
+          )
+          await loading.delete()
+          await message.channel.send(file=file, embed=embed)
+          os.remove("img.png")
 
-          ex = "(?<=p>Downloads: ).*(?=</p>)"
-          dls = re.findall(ex, response.text)[0]
+        else:
+          await loading.delete()
+          embed = discord.Embed(colour=discord.Colour(0x690a6))
 
-          ex = '(?<=<img class="is-rounded" src=").*(?=" alt="Profile picture for)'
-          
-          pp_url = re.findall(ex, response.text)
-          
+          embed.set_footer(text=footer(message), icon_url="https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
 
-        #  Hearts
+          embed.add_field(name="<:error:805061336185831434> Error <:error:805061336185831434>", value="This shortcut does not exist.\n\n")
 
-          ex = '(?<=<i class="fas fa-heart"></i></span>\n).*(?=\n</small>)'
-          hearts_list = re.findall(ex, response.text)
+          await message.channel.send(embed=embed)
 
-          hearts = 0
+    elif message.content.startswith("!rh user"):
+      user = message.content.replace("!rh user ", "")
+      if user:
+        user_url = f"https://routinehub.co/user/{user}"
+        response = requests.get(user_url)
+        if not "Error: Profile not found" in response.text:
 
-          for num in hearts_list:
-            num = int(num)
-            hearts += num
-          
-          ex = '(?<=<a class="pagination-link" href="\?page=).*(?=">)'
-          pages = re.findall(ex, response.text)
+          loading = await message.channel.send("Routinebot is loading your request")
 
-          for page in pages:
-            response = requests.get(f"{user_url}?page={page}")
+          async with message.channel.typing():
+
+            ex = "(?<=<p>Shortcuts: ).*(?=</p>)"
+            sc = re.findall(ex, response.text)[0]
+
+            ex = "(?<=<strong>).*(?=</strong>)"
+            username = re.findall(ex, response.text)[0]
+
+            ex = "(?<=p>Downloads: ).*(?=</p>)"
+            dls = re.findall(ex, response.text)[0]
+
+            ex = '(?<=<img class="is-rounded" src=").*(?=" alt="Profile picture for)'
+            
+            pp_url = re.findall(ex, response.text)
+            
+
+          #  Hearts
+
             ex = '(?<=<i class="fas fa-heart"></i></span>\n).*(?=\n</small>)'
             hearts_list = re.findall(ex, response.text)
+
+            hearts = 0
+
             for num in hearts_list:
               num = int(num)
               hearts += num
+            
+            ex = '(?<=<a class="pagination-link" href="\?page=).*(?=">)'
+            pages = re.findall(ex, response.text)
+
+            for page in pages:
+              response = requests.get(f"{user_url}?page={page}")
+              ex = '(?<=<i class="fas fa-heart"></i></span>\n).*(?=\n</small>)'
+              hearts_list = re.findall(ex, response.text)
+              for num in hearts_list:
+                num = int(num)
+                hearts += num
 
 
-          embed = discord.Embed(title=f"**{username}**", colour=discord.Colour(0x299d68), url=user_url)
+            embed = discord.Embed(title=f"**{username}**", colour=discord.Colour(0x299d68), url=user_url)
 
-          if len(pp_url) > 0:
-            embed.set_thumbnail(url=pp_url[0])
+            if len(pp_url) > 0:
+              embed.set_thumbnail(url=pp_url[0])
 
-          embed.set_footer(
-            text=f"{footer(message)}",
-            icon_url=
-            "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
+            embed.set_footer(
+              text=f"{footer(message)}",
+              icon_url=
+              "https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
 
-          embed.add_field(name="Information", value=f"Shortcuts: {sc}\nDownloads: {dls}\nHearts: {hearts}")
-        
-        await loading.delete()
-        await message.channel.send(embed=embed)
+            embed.add_field(name="Information", value=f"Shortcuts: {sc}\nDownloads: {dls}\nHearts: {hearts}")
+          
+          await loading.delete()
+          await message.channel.send(embed=embed)
 
-      else:
-        embed = discord.Embed(colour=discord.Colour(0x690a6))
+        else:
+          embed = discord.Embed(colour=discord.Colour(0x690a6))
 
-        embed.set_footer(text=footer(message), icon_url="https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
+          embed.set_footer(text=footer(message), icon_url="https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
 
-        embed.add_field(name="<:error:805061336185831434> Error <:error:805061336185831434>", value="User not found.\n\n")
+          embed.add_field(name="<:error:805061336185831434> Error <:error:805061336185831434>", value="User not found.\n\n")
 
-        await message.channel.send(embed=embed)
+          await message.channel.send(embed=embed)
 
-  elif message.content.startswith("!rh"):
-    embed = discord.Embed(colour=discord.Colour(0x690a6))
+    elif message.content.startswith("!rh"):
+      embed = discord.Embed(colour=discord.Colour(0x690a6))
 
-    embed.set_footer(
-      text=footer(message), icon_url="https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
+      embed.set_footer(
+        text=footer(message), icon_url="https://cdn.discordapp.com/avatars/792855846240518174/946eb210985c413d6b1429d49f9168fc.png?size=512")
 
-    embed.add_field(
-      name="<:error:805061336185831434> Error <:error:805061336185831434>", value="Invalid command, take a look to the help page with the '!rh help' command.\n\n"
-      )
+      embed.add_field(
+        name="<:error:805061336185831434> Error <:error:805061336185831434>", value="Invalid command, take a look to the help page with the '!rh help' command.\n\n"
+        )
 
-    await message.channel.send(embed=embed)
+      await message.channel.send(embed=embed)
 
 keep_alive()
 client.run(os.getenv("TOKEN"), bot=True)
