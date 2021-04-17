73558f7d17b60b5e7adc3a0976e1ed457051fe5e
elio27 <67876496+elio27@users.noreply.github.com>
2021-04-17 10:58:59 +0200

    Delete add_to_db.py

--- add_to_db.py
+++
@@ -1,9 +0,0 @@
-from replit import db
-
-for key in db.keys():
-    print(f"{key} : {db[key]}")
-
-id = input("Enter the id of the user : ")
-name = input("Enter the real username of the user : ")
-
-db[id] = name
